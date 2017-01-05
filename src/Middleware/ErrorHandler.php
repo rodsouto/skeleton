df17cc68f0d1bbeaaedb98bd5455224ef2d808e4
Rodrigo Souto <rodrigo@multiplica.com>
2017-01-05 15:10:41 -0300

    improve error reporting

--- src/Middleware/ErrorHandler.php
+++ src/Middleware/ErrorHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Middleware;
 
+use App\Config\Config;
 use App\Response\TwigResponse;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequest;
@@ -9,9 +10,11 @@ use Zend\Diactoros\ServerRequest;
 class ErrorHandler {
 
     private $twigResponse;
+    private $appConfig;
 
-    public function __construct(TwigResponse $twigResponse) {
+    public function __construct(TwigResponse $twigResponse, Config $appConfig) {
         $this->twigResponse = $twigResponse;
+        $this->appConfig = $appConfig;
     }
 
     public function __invoke(ServerRequest $request, Response $response, callable $next) {
@@ -19,7 +22,13 @@ class ErrorHandler {
             $response = $next($request, $response);
         } catch (\Exception $e) {
             // TODO: log
-            $response = $this->twigResponse->render('error.twig', ['error' => '500 INTERNAL SERVER ERROR']);
+            $vars = [
+                'error' => '500 INTERNAL SERVER ERROR',
+                'exception' => $e,
+                'showException' => $this->appConfig->environment === 'development',
+            ];
+
+            $response = $this->twigResponse->render('error.twig', $vars);
         }
 
         return $response;
